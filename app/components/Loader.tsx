034cf11621388abebbbf76f965b112ea36f2a4e0
Salah Ansari <salah.env@gmail.com>
2024-08-28 12:15:46 +0530

    Cosmatic changes

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -4,12 +4,13 @@ const SkeletonLoaderFocus = ({}: any) => {
   return (
     <div>
       <div className="animate-pulse">
-        <div className="h-48 bg-gray-300 rounded-md mb-4"></div>
-        <div className="h-6 bg-gray-300 rounded-md mb-2 w-3/4"></div>
         <div className="space-y-2">
-          <div className="h-4 bg-gray-300 rounded-md w-full"></div>
-          <div className="h-4 bg-gray-300 rounded-md w-5/6"></div>
-          <div className="h-4 bg-gray-300 rounded-md w-4/6"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
+          <div className="h-6 bg-gray-300 rounded-md w-full"></div>
         </div>
       </div>
     </div>
