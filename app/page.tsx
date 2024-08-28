f19e9707f7a87000614fa1b1f31c12e545ed5b04
Salah Ansari <salah.env@gmail.com>
2024-08-28 12:28:32 +0530

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -188,7 +188,7 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="flex justify-end mt-6">
+                <div className="mt-6">
                     <button 
                       className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
                       onClick={ () => {onCreateFocus()}}
