7b3da7707714579f548e0692e39126e3913c4cba
Salah Ansari <salah.env@gmail.com>
2024-09-01 14:53:15 +0530

    Added: floes

--- app/components/Timer.tsx
+++ app/components/Timer.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Spinner from './Spinner';
 
 // Function to format time as MM:SS
 const formatTime = (seconds: number) => {
@@ -7,79 +8,120 @@ const formatTime = (seconds: number) => {
   return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 };
 
-const Timer = ({timeInMinutes, onUpdateFocus, focus, toggleSuccessModal} : any) => {
+const Timer = ({timeInMinutes, isSaveFocusLoading, focusName, toggleSuccessModal, setEndTime, saveFocus} : any) => {
   const [time, setTime] = useState(timeInMinutes * 60); // Initial time in seconds
   const [progress, setProgress] = useState('100%');
   const [isActive, setIsActive] = useState(true);
+  const [showConfirmation, setShowConfirmation] = useState(false);
   
   // To keep track of the start time and elapsed time when the timer is running
-  const startTimeRef = useRef<number | null>(null);
+  const startTimeRef = useRef<number>(0);
   const animationFrameRef = useRef<number | null>(null);
-
+  const elapsedRef = useRef(0); // To track total elapsed time
+  
   useEffect(() => {
-    // Start the timer when component mounts or when `isActive` changes
-    if (!!isActive) {
-      startTimeRef.current = Date.now(); // Capture the start time
-      
+    if (isActive) {
+      if (!startTimeRef.current) {
+        startTimeRef.current = Date.now(); // Capture start time on first activation
+      } else {
+        // Adjust start time to account for the time already elapsed
+        console.log(elapsedRef.current);
+        startTimeRef.current = Date.now() - elapsedRef.current;
+      }
       const updateTimer = () => {
         const now = Date.now();
-        const elapsed = Math.floor((now - (startTimeRef.current || now)) / 1000); // Elapsed time in seconds
+        const elapsed = Math.floor((now - startTimeRef.current) / 1000); // Elapsed time in seconds
         const newTime = timeInMinutes * 60 - elapsed;
-
+  
         if (newTime <= 0) {
           setTime(0);
           setProgress('0%');
           toggleSuccessModal();
+          setEndTime(Date.now());
           return;
         } else {
           setTime(newTime);
           const progressVal = `${(100 / (timeInMinutes * 60) * newTime)}%`;
           setProgress(progressVal);
         }
-        animationFrameRef.current = requestAnimationFrame(updateTimer); // Call next frame
+  
+        animationFrameRef.current = requestAnimationFrame(updateTimer); // Continue the animation loop
       };
-
-      animationFrameRef.current = requestAnimationFrame(updateTimer); // Start the animation loop
+      animationFrameRef.current = requestAnimationFrame(updateTimer); // Start animation loop
     } else if (animationFrameRef.current) {
-      cancelAnimationFrame(animationFrameRef.current); // Stop animation when paused
+      // Pause the timer and track elapsed time
+      const now = Date.now();
+      elapsedRef.current += Math.floor((now - startTimeRef.current)); // Update elapsed time in seconds
+      cancelAnimationFrame(animationFrameRef.current); // Stop animation
     }
-
-    // Cleanup function to cancel the animation frame on unmount
+  
     return () => {
       if (animationFrameRef.current) cancelAnimationFrame(animationFrameRef.current);
     };
   }, [isActive, timeInMinutes]);
+  
 
   const onPausePlay = () => {
+    !isActive ? setEndTime(Date.now()) : null;
     setIsActive(!isActive);
   }
 
+  const onCloseBtnClick = () => {
+    setShowConfirmation(true);
+  }
+
+  const onYesClick = () => {
+    setEndTime(Date.now());
+    saveFocus();
+  }
+
   return (
-    <div>
-      <div className='text-center mb-3 font-medium text-gray-600 text-xl'>{focus?.name}</div>
-      <div className='text-center mb-2 font-bold text-blue-600 text-6xl'>{formatTime(time)}</div>
-      <div className="w-full h-6 bg-gray-300 rounded-full">
-        <div className="h-6 bg-blue-600 rounded-full flex items-center justify-center" style={{width: progress}}>
-          {parseInt(progress) > 5 && <span className="text-white font-bold">{Math.ceil(parseInt(progress))}%</span>}
+    <div>{ showConfirmation ?
+      <div>
+          <div className="text-center">
+              <svg className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+              </svg>
+              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure about quiting focus?</h3>
+              <button 
+                onClick={() => {
+                  onYesClick();
+                }}
+                disabled = {isSaveFocusLoading}
+                data-modal-hide="popup-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                  {isSaveFocusLoading ? <Spinner></Spinner> : "Yes, I'm sure"}
+              </button>
+              <button 
+                onClick={() => setShowConfirmation(false)}
+                data-modal-hide="popup-modal" 
+                type="button"
+                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+          </div>
+      </div> :
+      <div>
+        <div className="flex justify-end items-center">
+            <button 
+              className="text-gray-500 hover:text-gray-700" 
+              onClick={ () => {onCloseBtnClick()}}
+            >✖</button>
+        </div>
+        <div className='text-center mb-3 font-medium text-gray-600 text-xl'>{focusName}</div>
+        <div className='text-center mb-6 font-bold text-blue-600 text-6xl'>{formatTime(time)}</div>
+        <div className="w-full h-[36px] bg-gray-300 rounded-full">
+          <div className="h-[36px] bg-blue-600 rounded-full flex items-center justify-center" style={{width: progress}}>
+            {parseInt(progress) > 5 && <span className="text-white font-bold">{Math.ceil(parseInt(progress))}%</span>}
+          </div>
+        </div>
+        <div className="flex justify-center mt-6">
+          <button 
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-1"
+            onClick={onPausePlay}
+          >
+            {isActive ? 'Pause' : 'Resume'}
+          </button>
         </div>
       </div>
-      <div className="flex justify-center mt-6">
-        <button 
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-1"
-          onClick={onPausePlay}
-        >
-          {isActive ? 'Pause' : 'Resume'}
-        </button>
-        {!isActive ? (
-          <button 
-            className="bg-transparent text-blue-500 font-semibold hover:text-blue-700 py-2 px-4 border border-blue-500 hover:border-blue-700 rounded"
-            onClick={onUpdateFocus}
-          >
-            {'Complete'}
-          </button>
-        ) : null}
-      </div>
-    </div>
+    }</div>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import SuccessModal from "./components/SuccessModal";
 
 export default function Home() {
   const [focusName, setFocusName] = useState('');
+  const [startTime, setStartTime] = useState(Date.now());
+  const [endTime, setEndTime] = useState(Date.now());
   const [time, setTime] = useState('10');
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [showProgressModal, setShowProgressModal] = useState(false);
@@ -17,7 +19,7 @@ export default function Home() {
   const [focused, setFocused] = useState("00:00");
   const router = useRouter();
   const [isFocusLoading, setIsFocusLoading] = useState(true);
-  const [isCreateFocusLoading, setIsCreatFocusLoading] = useState(false);
+  const [isSaveFocusLoading, setIsSaveFocusLoading] = useState(false);
 
   useEffect(() => {
     getFocus();
@@ -64,8 +66,13 @@ export default function Home() {
       setIsFocusLoading(false);
     }
   }
-  const onCreateFocus = async () => {
-    setIsCreatFocusLoading(true);
+  const saveFocus = async () => {
+    const payload = {
+      name: focusName, 
+      startTime,
+      endTime
+    }
+    setIsSaveFocusLoading(true);
     try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/focus/create`,
         {
@@ -74,31 +81,32 @@ export default function Home() {
               'Content-Type': 'application/json',
             },
             method: "POST",
-            body: JSON.stringify({
-              name: focusName, 
-              startTime: new Date().getTime(),
-              endTime: new Date(new Date().getTime() + parseInt(time) * 60 * 1000)
-            }),
+            body: JSON.stringify(payload),
             credentials: 'include'
         }
       );
       const resData = await res.json();
       if(resData.success) {
-        setIsCreatFocusLoading(false);
         setActiveFocus(resData.data.focus);
         onCloseCreateFocusBtnClick();
         setShowProgressModal(true);
       }
       else {
-        setIsCreatFocusLoading(false);
-        console.log("Error while creating focus", resData);
         alert('Unable to create focus');
       }
     } catch (error) {
-      setIsCreatFocusLoading(false);
-      alert('Unable to create focus');
-      console.log(error);
+      alert(JSON.stringify(error));
+    } finally {
+      setIsSaveFocusLoading(false);
+      setShowProgressModal(false);
+      getFocus();
     }
+
+  }
+  const onCreateFocus = async () => {
+    setStartTime(Date.now());
+    onCloseCreateFocusBtnClick();
+    setShowProgressModal(true);
   }
   const formatDate = (date: any) => {
     date = new Date(date);
@@ -154,14 +162,17 @@ export default function Home() {
       }
       {
         showProgressModal ? 
-        <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-10">
             <div className="bg-neutral-100 rounded-lg shadow-lg w-full h-full p-6 md:max-w-lg md:h-auto md:rounded-lg relative">
               <div className="mt-4">
                   <Timer 
                     timeInMinutes = {parseInt(time)}
                     onUpdateFocus = {onUpdateFocus}
-                    focus = {activeFocus}
+                    focusName = {focusName}
                     toggleSuccessModal = {toggleSuccessModal}
+                    setEndTime = {setEndTime}
+                    saveFocus = {saveFocus}
+                    isSaveFocusLoading = {isSaveFocusLoading}
                   />  
               </div>
               </div>
@@ -211,9 +222,8 @@ export default function Home() {
                 <div className="mt-6">
                     <button 
                       className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
-                      disabled = {isCreateFocusLoading}
                       onClick={ () => {onCreateFocus()}}
-                    >{isCreateFocusLoading ? <Spinner></Spinner> : "Create"}</button>
+                    >{"Create"}</button>
                 </div>
             </div>
         </div>
