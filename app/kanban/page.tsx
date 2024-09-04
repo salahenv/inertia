cf818dd45612d1238cb927c690315dae9fe43eae
Salah Ansari <salah.env@gmail.com>
2024-09-05 00:44:56 +0530

    added: kanban

---
+++ app/kanban/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-100 p-4 min-h-screen">
+      Kanban
+    </div>
+  );
+}
