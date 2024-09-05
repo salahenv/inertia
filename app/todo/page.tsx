c4ae9d72892411d027062dabd0edf0c5c7e0ef90
Salah Ansari <salah.env@gmail.com>
2024-09-05 15:23:39 +0530

    rename it to todo

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Todo() {
   return (
     <div className="bg-neutral-100 p-4 min-h-screen">
-      Kanban
+      Todo
     </div>
   );
 }
