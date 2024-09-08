a6ecf0fcaa6b156755e31a5c1b7e86fe65603e4d
Salah Ansari <salah.env@gmail.com>
2024-09-09 02:23:05 +0530

    updated: colors

--- app/focus/page.tsx
+++ app/focus/page.tsx
@@ -436,11 +436,11 @@ export default function Home() {
                   const timeSpendInMinutes = Math.ceil((new Date(f.endTime).getTime() - new Date(f.startTime).getTime()) / (1000 * 60));
 
                   let bgClassName = '';
-                  if(timeSpendInMinutes > 61) bgClassName = 'bg-green-300';
-                  if(timeSpendInMinutes > 30 &&  timeSpendInMinutes < 61) bgClassName = 'bg-green-200';
-                  if(timeSpendInMinutes > 15 &&  timeSpendInMinutes < 31) bgClassName = 'bg-green-100';
-                  if(timeSpendInMinutes > 10 &&  timeSpendInMinutes < 16) bgClassName = 'bg-orange-300';
-                  if(timeSpendInMinutes > 0 &&  timeSpendInMinutes < 11) bgClassName = 'bg-red-400';
+                  if(timeSpendInMinutes > 50 &&  timeSpendInMinutes <= 120) bgClassName = 'bg-green-300';
+                  if(timeSpendInMinutes > 30 &&  timeSpendInMinutes <= 50) bgClassName = 'bg-green-200';
+                  if(timeSpendInMinutes > 20 &&  timeSpendInMinutes <= 30) bgClassName = 'bg-green-100';
+                  if(timeSpendInMinutes > 10 &&  timeSpendInMinutes <= 20) bgClassName = 'bg-orange-100';
+                  if(timeSpendInMinutes > 0 &&  timeSpendInMinutes <= 10) bgClassName = 'bg-red-100';
                   // if(timeSpendInMinutes <= 0) return;
 
                   return (
