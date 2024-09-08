cb145125146bc4acac8abbbd115d3afbe33b9a3d
Salah Ansari <salah.env@gmail.com>
2024-09-08 23:59:43 +0530

    added: icons

--- app/icons.tsx
+++ app/icons.tsx
@@ -33,11 +33,11 @@ const FocusIcon = ({ size = 32, color = 'rgb(37 99 235)' }) => (
   const DeleteIcon = ({ size = 32, color = 'rgb(37 99 235)' }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-1 -1 20 20" id="Delete-Bin-2--Streamline-Micro" height="20" width="20">
       <desc>Delete Bin 2 Streamline Icon: https://streamlinehq.com</desc>
-      <path stroke={color} stroke-linecap="round" stroke-linejoin="round" d="M1.35 4.5072h15.3" stroke-width="2"></path>
-      <path stroke={color} stroke-linecap="round" stroke-linejoin="round" d="M13.05 17.1h-8.1a1.8 1.8 0 0 1 -1.8 -1.8v-10.8h11.700000000000001v10.8a1.8 1.8 0 0 1 -1.8 1.8Z" stroke-width="2"></path>
-      <path stroke={color} stroke-linecap="round" stroke-linejoin="round" d="M5.774400000000001 4.5v-0.4122a3.1877999999999997 3.1877999999999997 0 1 1 6.3755999999999995 0V4.5" stroke-width="2"></path>
-      <path stroke={color} stroke-linecap="round" stroke-linejoin="round" d="M7.1802 8.1v5.4" stroke-width="2"></path>
-      <path stroke={color} stroke-linecap="round" stroke-linejoin="round" d="M10.827 8.1v5.4" stroke-width="2"></path>
+      <path stroke={color} strokeLinecap="round" stroke-linejoin="round" d="M1.35 4.5072h15.3" stroke-width="2"></path>
+      <path stroke={color} strokeLinecap="round" stroke-linejoin="round" d="M13.05 17.1h-8.1a1.8 1.8 0 0 1 -1.8 -1.8v-10.8h11.700000000000001v10.8a1.8 1.8 0 0 1 -1.8 1.8Z" stroke-width="2"></path>
+      <path stroke={color} strokeLinecap="round" stroke-linejoin="round" d="M5.774400000000001 4.5v-0.4122a3.1877999999999997 3.1877999999999997 0 1 1 6.3755999999999995 0V4.5" stroke-width="2"></path>
+      <path stroke={color} strokeLinecap="round" stroke-linejoin="round" d="M7.1802 8.1v5.4" stroke-width="2"></path>
+      <path stroke={color} strokeLinecap="round" stroke-linejoin="round" d="M10.827 8.1v5.4" stroke-width="2"></path>
     </svg>
  );
  const CompletedIcon = ({ size = 32, color = 'rgb(37 99 235)' }) => (
@@ -49,7 +49,7 @@ const FocusIcon = ({ size = 32, color = 'rgb(37 99 235)' }) => (
 </svg>
  )
 const TodoIcon = () => (
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="rgb(37 99 235)" stroke-linecap="round" stroke-linejoin="round" id="List-Check--Streamline-Tabler" height="32" width="32"><desc>List Check Streamline Icon: https://streamlinehq.com</desc><path d="M4.666666666666666 7.333333333333333 6.666666666666666 9.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="M4.666666666666666 15.333333333333332 6.666666666666666 17.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="M4.666666666666666 23.333333333333332 6.666666666666666 25.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="m14.666666666666666 8 12 0" stroke-width="2"></path><path d="m14.666666666666666 16 12 0" stroke-width="2"></path><path d="m14.666666666666666 24 12 0" stroke-width="2"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="rgb(37 99 235)" strokeLinecap="round" strokeLinejoin="round" id="List-Check--Streamline-Tabler" height="32" width="32"><desc>List Check Streamline Icon: https://streamlinehq.com</desc><path d="M4.666666666666666 7.333333333333333 6.666666666666666 9.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="M4.666666666666666 15.333333333333332 6.666666666666666 17.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="M4.666666666666666 23.333333333333332 6.666666666666666 25.333333333333332l3.333333333333333 -3.333333333333333" stroke-width="2"></path><path d="m14.666666666666666 8 12 0" stroke-width="2"></path><path d="m14.666666666666666 16 12 0" stroke-width="2"></path><path d="m14.666666666666666 24 12 0" stroke-width="2"></path></svg>
 )
 export {
     RightChevron,
@@ -61,4 +61,4 @@ export {
 }
 
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" id="Focus-2--Streamline-Tabler" height="16" width="16"><desc>Focus 2 Streamline Icon: https://streamlinehq.com</desc><path fill="#000000" d="M7.1875 7.5a0.3125 0.3125 0 1 0 0.625 0 0.3125 0.3125 0 1 0 -0.625 0" stroke-width="1"></path><path d="M3.125 7.5a4.375 4.375 0 1 0 8.75 0 4.375 4.375 0 1 0 -8.75 0" stroke-width="1"></path><path d="m7.5 1.875 0 1.25" stroke-width="1"></path><path d="m1.875 7.5 1.25 0" stroke-width="1"></path><path d="m7.5 11.875 0 1.25" stroke-width="1"></path><path d="m11.875 7.5 1.25 0" stroke-width="1"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 16 16" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" id="Focus-2--Streamline-Tabler" height="16" width="16"><desc>Focus 2 Streamline Icon: https://streamlinehq.com</desc><path fill="#000000" d="M7.1875 7.5a0.3125 0.3125 0 1 0 0.625 0 0.3125 0.3125 0 1 0 -0.625 0" strokeWidth="1"></path><path d="M3.125 7.5a4.375 4.375 0 1 0 8.75 0 4.375 4.375 0 1 0 -8.75 0" stroke-width="1"></path><path d="m7.5 1.875 0 1.25" stroke-width="1"></path><path d="m1.875 7.5 1.25 0" stroke-width="1"></path><path d="m7.5 11.875 0 1.25" stroke-width="1"></path><path d="m11.875 7.5 1.25 0" stroke-width="1"></path></svg>
