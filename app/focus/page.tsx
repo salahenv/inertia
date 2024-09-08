673342017fa85ab7c5ef808b97d069f8839df97c
Salah Ansari <salah.env@gmail.com>
2024-09-09 03:00:30 +0530

    added: colors

--- app/focus/page.tsx
+++ app/focus/page.tsx
@@ -436,7 +436,8 @@ export default function Home() {
                   const timeSpendInMinutes = Math.ceil((new Date(f.endTime).getTime() - new Date(f.startTime).getTime()) / (1000 * 60));
 
                   let bgClassName = '';
-                  if(timeSpendInMinutes > 50 &&  timeSpendInMinutes <= 120) bgClassName = 'bg-green-300';
+                  if(timeSpendInMinutes > 80 &&  timeSpendInMinutes <= 120) bgClassName = 'bg-green-400';
+                  if(timeSpendInMinutes > 50 &&  timeSpendInMinutes <= 80) bgClassName = 'bg-green-300';
                   if(timeSpendInMinutes > 30 &&  timeSpendInMinutes <= 50) bgClassName = 'bg-green-200';
                   if(timeSpendInMinutes > 20 &&  timeSpendInMinutes <= 30) bgClassName = 'bg-green-100';
                   if(timeSpendInMinutes > 10 &&  timeSpendInMinutes <= 20) bgClassName = 'bg-orange-100';
