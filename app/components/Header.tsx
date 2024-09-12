69c59d2bf4815f794c9dc558fcf7a434b526dd72
Salah Ansari <salah.env@gmail.com>
2024-09-12 16:08:25 +0530

    cosmatic

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = ({
   } : any) => {
 
   return (
-    <div className="font-bold z-100 shadow-2xl text-2xl sticky top-0 bg-neutral-100 px-4 py-2 border-b border-solid border-gray-400 text-blue-600">Inertia</div>
+    <div className="font-bold z-10 shadow text-2xl sticky top-0 bg-neutral-100 px-4 py-2 border-b border-solid border-gray-400 text-blue-600">Inertia</div>
   );
 };
 
