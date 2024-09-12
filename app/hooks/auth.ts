9d384f52d3d02a6776643bbe12a8c591fda39461
Salah Ansari <salah.env@gmail.com>
2024-09-13 01:34:47 +0530

    removed: console

--- app/hooks/auth.ts
+++ app/hooks/auth.ts
@@ -5,7 +5,6 @@ const useAuth = () => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const token = Cookies.get('token');
-      console.log('token', token);
       if (!token) {
         window.location.href = '/login';
       } else {
