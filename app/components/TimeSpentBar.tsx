605a8a558ff135030c677c3f05bfb440fa9880c4
Salah Ansari <salah.env@gmail.com>
2024-09-14 15:45:09 +0530

    removed: red color from analytics

--- app/components/TimeSpentBar.tsx
+++ app/components/TimeSpentBar.tsx
@@ -42,7 +42,6 @@ const TimeSpentBar = ({ data }: { data: any[] }) => {
   // Extended list of colors
   const colors = [
     'bg-green-400',
-    'bg-red-400',
     'bg-blue-400',
     'bg-yellow-400',
     'bg-purple-400',
@@ -54,7 +53,6 @@ const TimeSpentBar = ({ data }: { data: any[] }) => {
     'bg-yellow-500',
     'bg-green-500',
     'bg-blue-500',
-    'bg-red-500',
     'bg-purple-500',
     'bg-pink-500',
     'bg-teal-500',
@@ -64,7 +62,6 @@ const TimeSpentBar = ({ data }: { data: any[] }) => {
     'bg-yellow-600',
     'bg-green-600',
     'bg-blue-600',
-    'bg-red-600',
     'bg-purple-600',
     'bg-pink-600',
     'bg-teal-600',
