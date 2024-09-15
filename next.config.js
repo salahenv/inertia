bf5e86e02f8b2900bb088fa3f6ac56c1785d0eea
Salah Ansari <salah.env@gmail.com>
2024-09-15 17:09:15 +0530

    added: code for push notification

--- next.config.js
+++ next.config.js
@@ -1,8 +1,40 @@
-const withPWA = require('next-pwa')({
-    dest: 'public', // Output directory for service worker files
-    // disable: process.env.NODE_ENV === 'development', // Disable in development mode
-  });
-  
-  module.exports = withPWA({
-    // Other Next.js config options
-  });
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+          ],
+        },
+        {
+          source: '/sw.js',
+          headers: [
+            {
+              key: 'Content-Type',
+              value: 'application/javascript; charset=utf-8',
+            },
+            {
+              key: 'Cache-Control',
+              value: 'no-cache, no-store, must-revalidate',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; script-src 'self'",
+            },
+          ],
+        },
+      ]
+    },
+  }
