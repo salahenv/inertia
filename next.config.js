3004636fa30dafff8cc3d991f94bf7add4ad9382
Salah Ansari <salah.env@gmail.com>
2024-09-20 03:08:58 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,48 @@
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'strict-origin-when-cross-origin',
-            },
-          ],
-        },
-        {
-          source: '/sw.js',
-          headers: [
-            {
-              key: 'Content-Type',
-              value: 'application/javascript; charset=utf-8',
-            },
-            {
-              key: 'Cache-Control',
-              value: 'no-cache, no-store, must-revalidate',
-            },
-            {
-              key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self'",
-            },
-          ],
-        },
-      ]
-    },
-  }
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+        ],
+      },
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' https://api.salahenv.com; connect-src 'self' https://api.salahenv.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
