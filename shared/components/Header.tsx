45906e8c4863f837d17833e136015052db8b4bf7
Salah Ansari <salah.env@gmail.com>
2024-10-13 12:28:54 +0530

    added border in header

--- shared/components/Header.tsx
+++ shared/components/Header.tsx
@@ -35,7 +35,7 @@ const Header = ({
     }, []);
 
   return (
-    <div className='shadow z-10 sticky top-0 bg-white px-4 py-2 border-b border-solid border-gray-400 flex items-center'>
+    <div className='shadow z-10 sticky top-0 bg-white px-4 py-2 border-b border-solid border-gray-400 flex items-center justify-between'>
       <Link href = '/'>
         <div className="flex items-center mr-8">
           <Image 
@@ -51,12 +51,12 @@ const Header = ({
       <div className='flex'>
         <Link href = '/focus'>
           <div className={
-            `font-medium cursor-pointer text-xl ml-4 hover:text-blue-500 ${selectedMenu === 'focus' ? 'text-blue-600': 'text-gray-600'}`
+            `font-medium cursor-pointer text-xl ml-4 hover:text-blue-500 ${selectedMenu === 'focus' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
           }>Focus</div>
         </Link>
         <div className='relative group ml-4 z-50'>
           <div  className={
-            `font-medium cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'todo' ? 'text-blue-600': 'text-gray-600'}`
+            `font-medium cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'todo' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
           }>Todo</div>
           <div className='absolute hidden group-hover:block bg-white shadow p-2 rounded'>
             <Link href = '/todo'>
@@ -72,7 +72,7 @@ const Header = ({
         </div>
         <Link href = '/todo/routine'>
           <div className={
-            `font-medium ml-4 cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'routine' ? 'text-blue-600': 'text-gray-600'}`
+            `font-medium ml-4 cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'routine' ? 'text-blue-600 border-blue-600 border-b-4': 'text-gray-600'}`
           }>Routine</div>
         </Link>
       </div> : null
