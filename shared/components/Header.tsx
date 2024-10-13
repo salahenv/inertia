f3ec5eda1dfe7be513fddbafde91a0029aaad4fa
Salah Ansari <salah.env@gmail.com>
2024-10-13 20:47:54 +0530

    fixed: zindex issues in header

--- shared/components/Header.tsx
+++ shared/components/Header.tsx
@@ -35,7 +35,7 @@ const Header = ({
     }, []);
 
   return (
-    <div className='shadow z-10 sticky top-0 bg-white px-4 py-2 border-b border-solid border-gray-400 flex items-center justify-between'>
+    <div className='shadow z-20 sticky top-0 bg-white px-4 py-2 border-b border-solid border-gray-400 flex items-center justify-between'>
       <Link href = '/'>
         <div className="flex items-center mr-8">
           <Image 
@@ -51,28 +51,28 @@ const Header = ({
       <div className='flex'>
         <Link href = '/focus'>
           <div className={
-            `font-medium cursor-pointer text-xl ml-4 hover:text-blue-500 ${selectedMenu === 'focus' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
+            `font-medium cursor-pointer text-xl ml-4 hover:text-gray-800 ${selectedMenu === 'focus' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
           }>Focus</div>
         </Link>
-        <div className='relative group ml-4 z-50'>
+        <div className='relative group ml-4'>
           <div  className={
-            `font-medium cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'todo' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
+            `font-medium cursor-pointer text-xl hover:text-gray-800 ${selectedMenu === 'todo' ? 'text-blue-600 border-b-4 border-blue-600': 'text-gray-600'}`
           }>Todo</div>
-          <div className='absolute hidden group-hover:block bg-white shadow p-2 rounded'>
+          <div className='absolute hidden group-hover:block bg-white shadow px-4 py-2 rounded'>
             <Link href = '/todo'>
-              <div className='text-gray-600 mb-1'>Today</div>
+              <div className='text-gray-600 mb-1 hover:text-gray-800'>Today</div>
             </Link>
             <Link href = '/todo/completed'>
-              <div className='text-gray-600 mb-1'>Completed</div>
+              <div className='text-gray-600 mb-1 hover:text-gray-800'>Completed</div>
             </Link>
             <Link href = '/todo/archived'>
-              <div className='text-gray-600'>Archived</div>
+              <div className='text-gray-600 hover:text-gray-800'>Archived</div>
             </Link>
           </div>
         </div>
         <Link href = '/routine'>
           <div className={
-            `font-medium ml-4 cursor-pointer text-xl hover:text-blue-500 ${selectedMenu === 'routine' ? 'text-blue-600 border-blue-600 border-b-4': 'text-gray-600'}`
+            `font-medium ml-4 cursor-pointer text-xl hover:text-gray-800 ${selectedMenu === 'routine' ? 'text-blue-600 border-blue-600 border-b-4': 'text-gray-600'}`
           }>Routine</div>
         </Link>
       </div> : null
