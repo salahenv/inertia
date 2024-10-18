0a6ad06c887f40c35d163370dfd6fb11f326e708
Salah Ansari <salah.env@gmail.com>
2024-10-18 18:08:59 +0530

    upadeted

--- app/focus/page.tsx
+++ app/focus/page.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState, useContext } from "react";
 import FocusPage from "@/shared/pages/focus";
+import { Suspense } from 'react';
 
 export default function Home() {
 
   return (
-    <FocusPage />
+    <Suspense fallback={<div>Loading...</div>}>
+      <FocusPage />
+    </Suspense>
   );
 }
